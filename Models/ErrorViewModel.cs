86dfa7eff92c6bfd3fb0389fc213eb339c84624e
seheerst <seherst9@gmail.com>
2023-08-11 20:42:19 +0300

    initialize project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CourseSystemApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
