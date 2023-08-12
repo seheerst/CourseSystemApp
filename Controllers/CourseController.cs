4be4408ab9d6d63e6b4612b873fa58de0ea90d84
seheerst <seherst9@gmail.com>
2023-08-12 17:00:28 +0300

    feat: created action method

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -13,13 +13,13 @@ public class CourseController:Controller
       _context = context;
    }
  
-   
+   [HttpGet]
    public async Task<IActionResult> Index()
    {
-      var courses = await _context.Courses.ToListAsync();
+      var courses = await _context.Courses!.ToListAsync();
       return View(courses);
    } 
-   
+   [HttpGet]
    public IActionResult Create()
    {
       return View();
@@ -28,7 +28,93 @@ public class CourseController:Controller
    [HttpPost]
    public async Task<IActionResult> Create(Course course)
    {
-      _context.Courses.Add(course);
+      _context.Courses!.Add(course);
+      await _context.SaveChangesAsync();
+      return RedirectToAction("Index");
+   }
+
+   [HttpGet]
+
+   public async Task<IActionResult> Edit(int id)
+   {
+      if (id == null)
+      {
+         return NotFound();
+      }
+
+      var course = await _context.Courses.FindAsync(id);
+
+      if (course == null)
+      {
+         return NotFound();
+      }
+
+      return View(course);
+   }
+
+   [HttpPost]
+
+   public async Task<IActionResult> Edit(int id, Course course)
+   {
+      if (id != course.CourseId)
+      {
+         return NotFound();
+      }
+
+      if (ModelState.IsValid)
+      {
+         try
+         {
+            _context.Update(course);
+            await _context.SaveChangesAsync();
+         }
+         catch (DbUpdateConcurrencyException)
+         {
+            if (!_context.Courses.Any(s=> s.CourseId == course.CourseId))
+            {
+               return NotFound();
+            }
+            else
+            {
+               throw;
+            }
+         }
+
+         return RedirectToAction("Index");
+      }
+
+      return View(course);
+   }
+   
+    
+   [HttpGet]
+   public async Task<IActionResult> Delete(int id)
+   {
+      if (id==null)
+      {
+         return NotFound();
+      }
+
+      var course = await _context.Courses.FindAsync(id);
+      if (course == null)
+      {
+         return NotFound();
+      }
+
+      return View(course);
+   }
+
+   [HttpPost]
+
+   public async Task<IActionResult> Delete([FromForm]int? id)
+   {
+        
+      var course = await _context.Courses.FindAsync(id);
+      if (course==null)
+      {
+         return NotFound();
+      }
+      _context.Courses.Remove(course);
       await _context.SaveChangesAsync();
       return RedirectToAction("Index");
    }
